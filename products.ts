12921bd2208a2409d9f88299411d52a5991aff4b
Rakesh M <rakesh200023@gmail.com>
2023-12-13 22:56:46 +0530

    commit

---
+++ products.ts
@@ -0,0 +1,221 @@
+export const products = [
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/RUGBY_SHIRT_moist_9.png?crop=center&height=480&v=1685998420&width=480", // Replace with actual image URL
+    title: "Wireless Headphones",
+    description:
+      "Enjoy high-fidelity sound with these comfortable wireless headphones. Perfect for active lifestyles.",
+    price: 49.99,
+    discount: 10,
+    variants: ["Black", "White"],
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_9925_1_4dead4a6-f029-4ee3-88a2-dc9eaa138e76.jpg?crop=center&height=640&v=1714579125&width=640", // Replace with actual image URL
+    title: "Smart Watch",
+    description:
+      "Track your fitness goals and stay connected with this stylish smartwatch. Features heart rate monitoring and built-in GPS.",
+    price: 199.99,
+    variants: ["Black", "Silver", "Rose Gold"],
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_0310.jpg?crop=center&height=480&v=1695765206&width=480", // Replace with actual image URL
+    title: "Portable Speaker",
+    description:
+      "Fill any room with music using this powerful portable speaker. Features Bluetooth connectivity and long battery life.",
+    price: 39.99,
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/animebattleteefront.png?crop=center&height=480&v=1715796086&width=480", // Replace with actual image URL
+    title: "Fitness Tracker",
+    description:
+      "Stay motivated and on track with your fitness goals with this sleek fitness tracker. Tracks steps, distance, and calories burned.",
+    price: 24.99,
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_7309.jpg?crop=center&height=480&v=1695765404&width=480", // Replace with actual image URL
+    title: "Wireless Charger",
+    description:
+      "Charge your phone wirelessly with this convenient and stylish charger. Supports fast charging for compatible devices.",
+    price: 19.99,
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/RUGBY_SHIRT_moist_9.png?crop=center&height=480&v=1685998420&width=480", // Replace with actual image URL
+    title: "Wireless Headphones",
+    description:
+      "Enjoy high-fidelity sound with these comfortable wireless headphones. Perfect for active lifestyles.",
+    price: 49.99,
+    discount: 10,
+    variants: ["Black", "White"],
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_9925_1_4dead4a6-f029-4ee3-88a2-dc9eaa138e76.jpg?crop=center&height=640&v=1714579125&width=640", // Replace with actual image URL
+    title: "Smart Watch",
+    description:
+      "Track your fitness goals and stay connected with this stylish smartwatch. Features heart rate monitoring and built-in GPS.",
+    price: 199.99,
+    variants: ["Black", "Silver", "Rose Gold"],
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_0310.jpg?crop=center&height=480&v=1695765206&width=480", // Replace with actual image URL
+    title: "Portable Speaker",
+    description:
+      "Fill any room with music using this powerful portable speaker. Features Bluetooth connectivity and long battery life.",
+    price: 39.99,
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/animebattleteefront.png?crop=center&height=480&v=1715796086&width=480", // Replace with actual image URL
+    title: "Fitness Tracker",
+    description:
+      "Stay motivated and on track with your fitness goals with this sleek fitness tracker. Tracks steps, distance, and calories burned.",
+    price: 24.99,
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_7309.jpg?crop=center&height=480&v=1695765404&width=480", // Replace with actual image URL
+    title: "Wireless Charger",
+    description:
+      "Charge your phone wirelessly with this convenient and stylish charger. Supports fast charging for compatible devices.",
+    price: 19.99,
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/RUGBY_SHIRT_moist_9.png?crop=center&height=480&v=1685998420&width=480", // Replace with actual image URL
+    title: "Wireless Headphones",
+    description:
+      "Enjoy high-fidelity sound with these comfortable wireless headphones. Perfect for active lifestyles.",
+    price: 49.99,
+    discount: 10,
+    variants: ["Black", "White"],
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_9925_1_4dead4a6-f029-4ee3-88a2-dc9eaa138e76.jpg?crop=center&height=640&v=1714579125&width=640", // Replace with actual image URL
+    title: "Smart Watch",
+    description:
+      "Track your fitness goals and stay connected with this stylish smartwatch. Features heart rate monitoring and built-in GPS.",
+    price: 199.99,
+    variants: ["Black", "Silver", "Rose Gold"],
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_0310.jpg?crop=center&height=480&v=1695765206&width=480", // Replace with actual image URL
+    title: "Portable Speaker",
+    description:
+      "Fill any room with music using this powerful portable speaker. Features Bluetooth connectivity and long battery life.",
+    price: 39.99,
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/animebattleteefront.png?crop=center&height=480&v=1715796086&width=480", // Replace with actual image URL
+    title: "Fitness Tracker",
+    description:
+      "Stay motivated and on track with your fitness goals with this sleek fitness tracker. Tracks steps, distance, and calories burned.",
+    price: 24.99,
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_7309.jpg?crop=center&height=480&v=1695765404&width=480", // Replace with actual image URL
+    title: "Wireless Charger",
+    description:
+      "Charge your phone wirelessly with this convenient and stylish charger. Supports fast charging for compatible devices.",
+    price: 19.99,
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/RUGBY_SHIRT_moist_9.png?crop=center&height=480&v=1685998420&width=480", // Replace with actual image URL
+    title: "Wireless Headphones",
+    description:
+      "Enjoy high-fidelity sound with these comfortable wireless headphones. Perfect for active lifestyles.",
+    price: 49.99,
+    discount: 10,
+    variants: ["Black", "White"],
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_9925_1_4dead4a6-f029-4ee3-88a2-dc9eaa138e76.jpg?crop=center&height=640&v=1714579125&width=640", // Replace with actual image URL
+    title: "Smart Watch",
+    description:
+      "Track your fitness goals and stay connected with this stylish smartwatch. Features heart rate monitoring and built-in GPS.",
+    price: 199.99,
+    variants: ["Black", "Silver", "Rose Gold"],
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_0310.jpg?crop=center&height=480&v=1695765206&width=480", // Replace with actual image URL
+    title: "Portable Speaker",
+    description:
+      "Fill any room with music using this powerful portable speaker. Features Bluetooth connectivity and long battery life.",
+    price: 39.99,
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/animebattleteefront.png?crop=center&height=480&v=1715796086&width=480", // Replace with actual image URL
+    title: "Fitness Tracker",
+    description:
+      "Stay motivated and on track with your fitness goals with this sleek fitness tracker. Tracks steps, distance, and calories burned.",
+    price: 24.99,
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_7309.jpg?crop=center&height=480&v=1695765404&width=480", // Replace with actual image URL
+    title: "Wireless Charger",
+    description:
+      "Charge your phone wirelessly with this convenient and stylish charger. Supports fast charging for compatible devices.",
+    price: 19.99,
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/RUGBY_SHIRT_moist_9.png?crop=center&height=480&v=1685998420&width=480", // Replace with actual image URL
+    title: "Wireless Headphones",
+    description:
+      "Enjoy high-fidelity sound with these comfortable wireless headphones. Perfect for active lifestyles.",
+    price: 49.99,
+    discount: 10,
+    variants: ["Black", "White"],
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_9925_1_4dead4a6-f029-4ee3-88a2-dc9eaa138e76.jpg?crop=center&height=640&v=1714579125&width=640", // Replace with actual image URL
+    title: "Smart Watch",
+    description:
+      "Track your fitness goals and stay connected with this stylish smartwatch. Features heart rate monitoring and built-in GPS.",
+    price: 199.99,
+    variants: ["Black", "Silver", "Rose Gold"],
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_0310.jpg?crop=center&height=480&v=1695765206&width=480", // Replace with actual image URL
+    title: "Portable Speaker",
+    description:
+      "Fill any room with music using this powerful portable speaker. Features Bluetooth connectivity and long battery life.",
+    price: 39.99,
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/animebattleteefront.png?crop=center&height=480&v=1715796086&width=480", // Replace with actual image URL
+    title: "Fitness Tracker",
+    description:
+      "Stay motivated and on track with your fitness goals with this sleek fitness tracker. Tracks steps, distance, and calories burned.",
+    price: 24.99,
+  },
+  {
+    image:
+      "https://moistglobal.com/cdn/shop/files/IMG_7309.jpg?crop=center&height=480&v=1695765404&width=480", // Replace with actual image URL
+    title: "Wireless Charger",
+    description:
+      "Charge your phone wirelessly with this convenient and stylish charger. Supports fast charging for compatible devices.",
+    price: 19.99,
+  },
+];
+
+// for (let i = 0; i < products.length; i++) {
+//   products[i].id = i + 1;
+// }
